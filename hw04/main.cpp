10361eb981f8b2710312af57cd6bf4fce076a088
NeuroDuck <cliff2401@cliffolling.com>
2015-05-02 17:31:46 -0700

    Add initial version of Report, and updated Keil settings files.

--- hw04/main.cpp
+++ hw04/main.cpp
@@ -4,10 +4,10 @@ DigitalOut myled(LED1);
 
 int main() 
 {
-    while(1) 
+    while (1) 
     {
 //        myled = 1;
-        wait( 1.2);
+        wait( 1.7);
         myled = 0;
         wait( 0.005);
     }
