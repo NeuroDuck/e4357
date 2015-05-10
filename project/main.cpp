5d300730aff7d5bb21dec7f68ba7176caa81186e
NeuroDuck <cliff2401@cliffolling.com>
2015-05-10 11:42:23 -0700

    Add some notes on my latest RPC findings.

--- project/main.cpp
+++ project/main.cpp
@@ -1,5 +1,15 @@
+//
+// Keil's 32kByte-limit means that I can't compile in all at the same
+// time the Libraries/Functions for:
+//    m3pi
+//    RPCFunction/ReadRange
+//    RPCVariable
+//
+// So I comment out the ones I'm not debugging into, and leave
+// uncommented-out just the one that I am debugging.
+//
 #include "mbed.h"
-// #include "m3pi.h"
+#include "m3pi/m3pi.h"
 #include "SerialRPCInterface.h"
 
 SerialRPCInterface rpcInterface( USBTX, USBRX);
@@ -9,10 +19,13 @@ RPCFunction RangeFinder( &ReadRange, "RangeFinder");
 
 void ReadRange( char* input, char* output)
 {
-    //Format the output of the srf08 into the output string
-    sprintf( output, "%f", 42.42);
+    // Format the output of the srf08 into the output string.
+		//
+		strcpy( output, "abc");
+//    sprintf( output, "%f", 42.42);
 }
 
+/*
 // First create the variables you wish to use.
 //
 float f = 42;
@@ -24,10 +37,27 @@ char  c = 'b';
 RPCVariable<float> rpc_f( &f, "f");
 RPCVariable<int>   rpc_i( &i, "i");
 RPCVariable<char>  rpc_c( &c, "c");
+*/
+
+// m3pi mpi;
 
 int main() 
 {
-	rpcInterface.Enable();
+	// I'm happy to report that typing in /m3pi/battery in my Serial 
+	// Terminal program did print out the battery value, and typing 
+	// in /m3pi/left did make the Robot spin left.
+	//
+	// Meaning that the m3pi Library's approach to register-function-
+	// for-RPC is working.
+	//
+	// Since the SerialRPCInterface Library's RPCFunction.cpp's 
+  // registration approach isn't working, I'll modify it to match
+	// the mp3i Library's approach, and then hopefully we'll have 
+	// a RPCFunction()-registration Class that works.
+
+//		mpi.locate( 0,1);
+//		char msg[] = "USB RPC";
+//    mpi.print( msg, strlen( msg));
 	
 	int a = 0;		// To have a line to set a breakpoint on.
 	  
