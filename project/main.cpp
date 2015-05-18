b1f9cef4221861ebe986761cc18cade95b9febca
NeuroDuck <cliff2401@cliffolling.com>
2015-05-18 16:18:03 -0700

    Initial version of JoyStick-driving-Robot code.

--- project/main.cpp
+++ project/main.cpp
@@ -10,36 +10,151 @@
 //
 #include "mbed.h"
 #include "m3pi/m3pi.h"
-#include "SerialRPCInterface.h"
 
-// SerialRPCInterface rpcInterface( USBTX, USBRX);
-SerialRPCInterface rpcInterface( p28, p27);
-/*
-void ReadRange( char* input, char* output);
-RPCFunction RangeFinder( &ReadRange, "RangeFinder");
+m3pi   mpi;
+Serial pc(   USBTX, USBRX); // tx, rx
+Serial xBee( p28,   p27);
 
-void ReadRange( char* input, char* output)
+int signI( int i)
 {
-    // Format the output of the srf08 into the output string.
-		//
-		strcpy( output, "abc");
-//    sprintf( output, "%f", 42.42);
+  if (i < 0)
+    return -1;
+
+  if (i > 0)
+    return 1;
+
+  return 0;      
 }
 
-// First create the variables you wish to use.
-//
-float f = 42;
-int   i = 43;
-char  c = 'b';
+int maxI( int i, int j)
+{
+	if (i > j)
+		return i;
 
-// Then attach them to an RPCVariable Object.
-//
-RPCVariable<float> rpc_f( &f, "f");
-RPCVariable<int>   rpc_i( &i, "i");
-RPCVariable<char>  rpc_c( &c, "c");
-*/
+  return j;
+}
 
-m3pi mpi;
+int minI( int i, int j)
+{
+	if (i > j)
+		return j;
+
+  return i;
+}
+
+const int numRadiusBands  = 8;
+const int numThetaSectors = 8;
+
+int prevTheta = 0;	
+
+void processCmd( char* cmd)
+{
+	char *rStr, *thetaStr;
+	char delims[] = " \t";
+	
+	char* firstDelim = strpbrk( cmd, delims);
+	
+	if (firstDelim == NULL)
+		return;																// Malformed, so bail.
+
+	rStr     = strtok( cmd,  delims);
+	thetaStr = strtok( NULL, delims);
+	
+//	pc.printf( "rStr = '%s', thetaStr = '%s'\r", rStr, thetaStr);
+	
+	// We seem to be receiving doubled characters for theta, so for now
+	// just stomp the 2nd one and declare victory.
+	//
+	thetaStr[1] = '\0';
+	int thetaVal = thetaStr[0] - '0';
+	
+	if (thetaVal < 0 || thetaVal > numThetaSectors - 1)
+		return;																// Malformed, so bail.		
+		
+	int rVal    = atoi( rStr);
+	int absRval = abs(  rVal);
+	
+	if ((absRval < 0   || absRval > numRadiusBands - 1) &&
+			(absRval < 100 || absRval > 200))
+		return;																// Malformed, so bail.
+	
+	int forwardSpeed = 0;										// Not moving yet.
+	int rotateDir    = 0;										// No rotation yet.
+
+	// absRval >= 100 means full-scale JoyStick deflection, which we
+	// take to mean Rotation rather than movement.
+	//
+	if (absRval >= 100)
+	{
+//		pc.printf( "Processing full-scale theta = '%d'\r", thetaVal);
+		
+		// When we're told that theta changed from 2 to 7, it's ambiguous,
+		// because we don't know if it changed in the cw or ccw direction.
+		// To resolve the ambiguity, we'll calculate both differences.
+		//
+		// The positive one will be in the ccw direction.
+		//
+		// For the negative one, we'll add numThetaSectors.
+		//
+		int negDiff, posDiff;
+		int localPrevTheta = prevTheta;
+		
+		int diff = thetaVal - prevTheta;
+		
+//		pc.printf( "diff = %d\r", diff);
+		
+		if (diff < 0)
+		{
+			negDiff =  diff + numThetaSectors;
+			posDiff = -diff;
+		}
+		else if (diff > 0)
+		{
+			negDiff = numThetaSectors - diff;
+			posDiff = diff;
+		}
+		else
+			return;					// Nothing to see here, move along, move along.
+
+//		pc.printf( "negDiff = %d, posDiff = %d\r", negDiff, posDiff);
+		
+		// Finally, rotateDir = the sign of the smaller of the absValue of 
+		// cwVal and ccwVal.
+		
+		int signDiff = signI( diff);
+		rotateDir = (negDiff < posDiff) ? -signDiff : signDiff;
+		
+		prevTheta = thetaVal;
+	}
+	else
+		forwardSpeed = absRval;
+
+	if (absRval >= 100)
+	{
+		pc.printf( "rotateDir = %d\r", rotateDir);
+		
+		const float rotationRate = 0.1;
+		
+		if (rotateDir > 0)
+			mpi.left( rotationRate);
+		else
+			mpi.right( rotationRate);
+	}
+	else
+	{
+		pc.printf( "forwardSpeed = %d\r", forwardSpeed);
+		
+		if (thetaVal >= numThetaSectors / 2)
+			forwardSpeed *= -1;
+		
+		if (forwardSpeed != 0)
+			mpi.forward( forwardSpeed / 10.0);
+		else
+			mpi.stop();
+	}
+}
+
+#define BUFLEN 80
 
 int main() 
 {
@@ -56,11 +171,43 @@ int main()
 	// a RPCFunction()-registration Class that works.
 
 		mpi.locate( 0,1);
-		char msg[] = "USB_RPC";
+		char msg[] = "JOYSTICK";
     mpi.print( msg, strlen( msg));
 	
-   	int a = 0;		// To have a line to set a breakpoint on.
+		char buf[BUFLEN] = "";
+		int bufLen = 0;
 	
-//		mpi.stop();
-	  
+		pc.baud( 57600);
+		xBee.baud( 57600);
+		
+		int count = 0;
+	
+		while (1)
+		{
+        if (!xBee.readable())
+					continue;
+
+				char c = xBee.getc();
+				pc.putc( c);
+				
+				if (bufLen >= BUFLEN || c == '\r' || c == '\n')
+				{
+					processCmd( buf);
+					bufLen = 0;
+					pc.printf( "\r");
+				}
+				else						
+					buf[bufLen++] = c;
+				
+				count++;
+				
+				if (count % 100 == 0)
+				{
+					mpi.locate( 0,0);
+					sprintf( msg, "B=%4dmV", int( mpi.battery() * 1000));
+					mpi.print( msg, strlen( msg));
+				}
+		}
+		
+		return 1;
 }
